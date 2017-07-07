55f8d9ba4475eb42ee3245b7cfb1800b4377ccc3
Tamirys Virgulino <tamygemeas@gmail.com>
2017-07-07 09:23:18 -0300

    Alterações

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <div>
-    <h1> {{ titulo }} </h1>
-    <ul>
-        <li v-for="foto of fotos">
-            <img :src="foto.url" :alt="foto.titulo">
+  <div class="corpo">
+    <h1 class="centralizado"> {{ titulo }} </h1>
+    <ul class="lista-fotos">
+        <li class="lista-fotos-item" v-for="foto of fotos">
+          <meu-painel :titulo="foto.titulo">
+            <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo">
+          </meu-painel>
         </li>
     </ul>
   </div>
@@ -28,4 +30,28 @@ export default {
 </script>
 
 <style>
+
+  .corpo{
+    font-family: Helvetica, sans-serif;
+    width: 96%;
+    margin: 0 auto;
+  }
+
+  .centralizado{
+    text-align: center;
+  }
+
+  .lista-fotos{
+    list-style: none;
+  }
+
+  .lista-fotos .lista-fotos-item{
+    display: inline-block;
+  }
+
+  .imagem-responsiva{
+    width: 100%;
+  }
+
+
 </style>
