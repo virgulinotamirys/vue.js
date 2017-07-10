6f73d921195f6a07dc1fecbc9454857a9ebf58bc
Tamirys Virgulino <tamygemeas@gmail.com>
2017-07-10 08:50:00 -0300

    component cadastro

--- src/main.js
+++ src/main.js
@@ -5,9 +5,11 @@ import App from './App.vue'
 
 // importando o módulo
 import VueResource from 'vue-resource';
+import VueRouter from 'vue-router';
 
 // registrando o módulo/plugin no global view object
 Vue.use(VueResource);
+Vue.use(VueRouter);
 
 new Vue({
   el: '#app',
